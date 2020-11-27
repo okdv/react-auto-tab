776b153f4045d6988c51d784e44696cadbff17c9
okdv <okd@otho.dev>
2020-11-26 18:38:42 -0600

    Updates

---
+++ precache-manifest.38bd7f0ef8622c90505ca4b0c59db235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14697a1e1a504ab6d82fa953c6f0a2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2daf2c7cd33d8146c619",
+    "url": "./static/css/main.984e5aef.chunk.css"
+  },
+  {
+    "revision": "2d6be8476fbc60ffa4ae",
+    "url": "./static/js/2.cc488cd2.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "./static/js/2.cc488cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2daf2c7cd33d8146c619",
+    "url": "./static/js/main.1fb27263.chunk.js"
+  },
+  {
+    "revision": "d8cdade0f45cd8bbd7f5",
+    "url": "./static/js/runtime-main.06383201.js"
+  },
+  {
+    "revision": "0d5dcb635fb2ad80d472ad74b314e2c6",
+    "url": "./static/media/bee-conservancy-logo.0d5dcb63.png"
+  }
+]);
